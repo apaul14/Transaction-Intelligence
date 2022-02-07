7b350e184cdd91ca5226e28a15b4bf7ed5b39899
Alex Paul <al.9thousand@gmail.com>
2022-02-07 16:16:44 -0500

    feat: server receives json and stores in mockDataStore

--- api/uploadTransactions.js
+++ api/uploadTransactions.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  const email = req.body.email
-  const response = `you hit the upload transactions endpoint ${email}!`
-  res.status(200).send(response)
+const mockDataStore = []
+
+// router.get('/', async (req, res, next) => {
+//   const email = req.body.email
+//   const response = `you hit the upload transactions endpoint ${email}!`
+//   res.status(200).send(response)
+// })
+
+router.post('/', async (req, res, next) => {
+  try {
+    console.log(req.body)
+    mockDataStore.push(req.body)
+    
+    res.status(201).send(mockDataStore)
+  } catch (error) {
+    next(error)
+  }
+  
 })
+// console.log(JSON.parse(req.body))
+    // const data = JSON.parse(req.body)
+    //mockDataStore.concat(data)
+    // const email = req.body.email
+    // const response = `you hit the upload transactions endpoint ${email}!`
 
 module.exports = router
